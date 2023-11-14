764764a92e285b4375d9a2297e0f2a04e32cf80c
RoyNyaga <nyagaandreroy@gmail.com>
2023-11-14 09:55:28 +0100

    update some pages

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -67,76 +67,76 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div class="featured section">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-4">
-              <div class="left-image"> 
+      <div className="featured section">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="left-image">
                 <img src="../../images/featured.jpg" alt="missing-image" />
-                  <a href="property-details.html"><img src="images/featured-icon.png" alt="" style={{ maxWidth: "60px", padding: "0px" }} /></a>
+                <a href="property-details.html"><img src="images/featured-icon.png" alt="" style={{ maxWidth: "60px", padding: "0px" }} /></a>
               </div>
             </div>
-            <div class="col-lg-5">
-              <div class="section-heading">
+            <div className="col-lg-5">
+              <div className="section-heading">
                 <h6>| Featured</h6>
                 <h2>Best Appartment &amp; Sea view</h2>
               </div>
-              <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
-                  <h2 class="accordion-header" id="headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingOne">
+                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                       Best useful links ?
                     </button>
                   </h2>
-                  <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                  <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
                       Get <strong>the best villa</strong> website template in HTML CSS and Bootstrap for your business. TemplateMo provides you the <a href="https://www.google.com/search?q=best+free+css+templates" target="_blank">best free CSS templates</a> in the world. Please tell your friends about it.</div>
                   </div>
                 </div>
-                <div class="accordion-item">
-                  <h2 class="accordion-header" id="headingTwo">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingTwo">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
                       How does this work ?
                     </button>
                   </h2>
-                  <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                  <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
                       Dolor <strong>almesit amet</strong>, consectetur adipiscing elit, sed doesn't eiusmod tempor incididunt ut labore consectetur <code>adipiscing</code> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </div>
                   </div>
                 </div>
-                <div class="accordion-item">
-                  <h2 class="accordion-header" id="headingThree">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                <div className="accordion-item">
+                  <h2 className="accordion-header" id="headingThree">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
                       Why is Villa Agency the best ?
                     </button>
                   </h2>
-                  <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                    <div class="accordion-body">
+                  <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div className="accordion-body">
                       Dolor <strong>almesit amet</strong>, consectetur adipiscing elit, sed doesn't eiusmod tempor incididunt ut labore consectetur <code>adipiscing</code> elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="col-lg-3">
-              <div class="info-table">
+            <div className="col-lg-3">
+              <div className="info-table">
                 <ul>
                   <li>
                     <img src="../../images/info-icon-01.png" alt="" style={{ maxWidth: "52px" }} />
-                      <h4>250 m2<br /><span>Total Flat Space</span></h4>
+                    <h4>250 m2<br /><span>Total Flat Space</span></h4>
                   </li>
                   <li>
                     <img src="../../images/info-icon-02.png" alt="" style={{ maxWidth: "52px" }} />
-                      <h4>Contract<br /><span>Contract Ready</span></h4>
+                    <h4>Contract<br /><span>Contract Ready</span></h4>
                   </li>
                   <li>
                     <img src="../../images/info-icon-03.png" alt="" style={{ maxWidth: "52px" }} />
-                      <h4>Payment<br /><span>Payment Process</span></h4>
+                    <h4>Payment<br /><span>Payment Process</span></h4>
                   </li>
                   <li>
                     <img src="../../images/info-icon-04.png" alt="" style={{ maxWidth: "52px" }} />
-                      <h4>Safety<br /><span>24/7 Under Control</span></h4>
+                    <h4>Safety<br /><span>24/7 Under Control</span></h4>
                   </li>
                 </ul>
               </div>
@@ -144,8 +144,152 @@ const HomePage = () => {
           </div>
         </div>
       </div>
-    </div>
-  )
+
+      <div className="fun-facts">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="wrapper">
+                <div className="row">
+                  <div className="col-lg-4">
+                    <div className="counter">
+                      <h2 className="timer count-title count-number">10</h2>
+                      <p className="count-text ">Buildings<br />Finished Now</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="counter">
+                      <h2 className="timer count-title count-number">10</h2>
+                      <p className="count-text ">Years<br />Experience</p>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="counter">
+                      <h2 className="timer count-title count-number">10</h2>
+                      <p className="count-text ">Awwards<br />Won 2023</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section best-deal">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="section-heading">
+                <h6>| Best Deal</h6>
+                <h2>Find Your Best Deal Right Now!</h2>
+              </div>
+            </div>
+            <div className="col-lg-12">
+              <div className="tabs-content">
+                <div className="row">
+                  <div className="nav-wrapper ">
+                    <ul className="nav nav-tabs" role="tablist">
+                      <li className="nav-item" role="presentation">
+                        <button className="nav-link active" id="appartment-tab" data-bs-toggle="tab" data-bs-target="#appartment" type="button" role="tab" aria-controls="appartment" aria-selected="true">Appartment</button>
+                      </li>
+                      <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="villa-tab" data-bs-toggle="tab" data-bs-target="#villa" type="button" role="tab" aria-controls="villa" aria-selected="false">Villa House</button>
+                      </li>
+                      <li className="nav-item" role="presentation">
+                        <button className="nav-link" id="penthouse-tab" data-bs-toggle="tab" data-bs-target="#penthouse" type="button" role="tab" aria-controls="penthouse" aria-selected="false">Penthouse</button>
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="tab-content" id="myTabContent">
+                    <div className="tab-pane fade show active" id="appartment" role="tabpanel" aria-labelledby="appartment-tab">
+                      <div className="row">
+                        <div className="col-lg-3">
+                          <div className="info-table">
+                            <ul>
+                              <li>Total Flat Space <span>185 m2</span></li>
+                              <li>Floor number <span>26th</span></li>
+                              <li>Number of rooms <span>4</span></li>
+                              <li>Parking Available <span>Yes</span></li>
+                              <li>Payment Process <span>Bank</span></li>
+                            </ul>
+                          </div>
+                        </div>
+                        <div className="col-lg-6">
+                          <img src="../../images/deal-01.jpg" alt="" />
+                        </div>
+                        <div className="col-lg-3">
+                          <h4>Extra Info About Property</h4>
+                          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.
+                            <br /><br />When you need free CSS templates, you can simply type TemplateMo in any search engine website. In addition, you can type TemplateMo Portfolio, TemplateMo One Page Layouts, etc.</p>
+                              <div className="icon-button">
+                                <a href="property-details.html"><i className="fa fa-calendar"></i> Schedule a visit</a>
+                              </div>
+                            </div>
+                        </div>
+                      </div>
+                      <div className="tab-pane fade" id="villa" role="tabpanel" aria-labelledby="villa-tab">
+                        <div className="row">
+                          <div className="col-lg-3">
+                            <div className="info-table">
+                              <ul>
+                                <li>Total Flat Space <span>250 m2</span></li>
+                                <li>Floor number <span>26th</span></li>
+                                <li>Number of rooms <span>5</span></li>
+                                <li>Parking Available <span>Yes</span></li>
+                                <li>Payment Process <span>Bank</span></li>
+                              </ul>
+                            </div>
+                          </div>
+                          <div className="col-lg-6">
+                            <img src="../../images/deal-02.jpg" alt="" />
+                          </div>
+                          <div className="col-lg-3">
+                            <h4>Detail Info About Villa</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse.
+                             <br /><br />Swag fanny pack lyft blog twee. JOMO ethical copper mug, succulents typewriter shaman DIY kitsch twee taiyaki fixie hella venmo after messenger poutine next level humblebrag swag franzen.</p>
+                              <div className="icon-button">
+                                <a href="property-details.html"><i className="fa fa-calendar"></i> Schedule a visit</a>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div className="tab-pane fade" id="penthouse" role="tabpanel" aria-labelledby="penthouse-tab">
+                          <div className="row">
+                            <div className="col-lg-3">
+                              <div className="info-table">
+                                <ul>
+                                  <li>Total Flat Space <span>320 m2</span></li>
+                                  <li>Floor number <span>34th</span></li>
+                                  <li>Number of rooms <span>6</span></li>
+                                  <li>Parking Available <span>Yes</span></li>
+                                  <li>Payment Process <span>Bank</span></li>
+                                </ul>
+                              </div>
+                            </div>
+                            <div className="col-lg-6">
+                              <img src="../../images/deal-03.jpg" alt="" />
+                            </div>
+                            <div className="col-lg-3">
+                              <h4>Extra Info About Penthouse</h4>
+                              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, do eiusmod tempor pack incididunt ut labore et dolore magna aliqua quised ipsum suspendisse. 
+                              <br /><br />Swag fanny pack lyft blog twee. JOMO ethical copper mug, succulents typewriter shaman DIY kitsch twee taiyaki fixie hella venmo after messenger poutine next level humblebrag swag franzen.</p>
+                                <div className="icon-button">
+                                  <a href="property-details.html"><i className="fa fa-calendar"></i> Schedule a visit</a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          )
 }
 
-export default HomePage;
+          export default HomePage;
